c8f75229eb890e2dd9ed56fbaf1e1d746a377aba
saurav123anand <123sauravanand@gmail.com>
2024-03-25 02:13:48 +0530

    Tree, BST and avl tree added

---
+++ trees/trees/src/BST.java
@@ -0,0 +1,122 @@
+public class BST {
+    public class Node{
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+        public Node(int value){
+           this.value=value;
+        }
+        public int getValue(){
+            return value;
+        }
+    }
+    private Node root;
+    public BST(){
+
+    }
+    public int height(Node node){
+        if(node==null){
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty(){
+        return root==null;
+    }
+    public  void display(){
+        display(root,"Root node");
+    }
+    public void display(Node node,String details){
+        if (node==null){
+            return;
+        }
+        System.out.println(details+" is "+node.getValue());
+        display(node.left,"Left child of "+node.value);
+        display(node.right,"Right child of "+node.value);
+    }
+    public void insert(int value){
+       root=insert(value,root);
+    }
+    private Node insert(int value,Node node){
+        if (node==null){
+            node=new Node(value);
+            return node;
+        }
+        if (value<node.value){
+            node.left=insert(value,node.left);
+        }
+        if (value>node.value){
+            node.right=insert(value,node.right);
+        }
+        node.height=Math.max(height(node.left),height(node.right))+1;
+        return node;
+    }
+    public boolean isBalanced(){
+        return balanced(root);
+    }
+    private boolean balanced(Node node){
+        if (node==null){
+            return true;
+        }
+        return Math.abs(height(node.left)-height(node.right))<=1 && balanced(node.left) && balanced(node.right);
+    }
+    public void populate(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            insert(nums[i]);
+        }
+    }
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    // preorder
+    public void preOrder(){
+        preOrder(root);
+    }
+    public void preOrder(Node node){
+        if (node==null){
+            return;
+        }
+        System.out.print(node.value+" ");
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    public void inOrder(Node node){
+        if (node==null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value+" ");
+        inOrder(node.right);
+
+    }
+    public void postOrder(){
+        postOrder(root);
+    }
+    public void postOrder(Node node){
+        if (node==null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value+" ");
+
+    }
+}
